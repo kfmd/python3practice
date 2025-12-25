87b65b42a4e6f2bfd71a1fa19aef1b33bf1141d6
KFMD <kfmd@protonmail.com>
2025-12-25 19:50:26 +0700

    how to write single line and paragraph

--- Mosh YT/learning-log.md
+++ Mosh YT/learning-log.md
@@ -1,2 +1,2 @@
 last watched minute 
-22:41 
+33:01
---
+++ Mosh YT/single-or-double-quote.py
@@ -0,0 +1,10 @@
+#course = "Python for "beginner""
+#kode diatas menghasilkan error karena python bingung ga bisa  interpretasi beginner 
+#yang benar pakai single quote :
+course_one_liner = 'Python for "beginner"'
+print(course_one_liner)
+print()
+course_paragraph = ''' 
+This program prompts the user for a weight in pounds and then performs the conversion. It's a great beginner exercise for learning input/output and data types.
+'''
+print(course_paragraph)

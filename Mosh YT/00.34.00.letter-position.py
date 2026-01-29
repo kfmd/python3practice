7517711b162d8388ae7f17204e609e638bb88fd1
KFMD <kfmd@protonmail.com>
2026-01-30 05:59:15 +0700

    Learned index, how to print certain character from string, or certain part of sentence using number 1,-1,[1,-1]

---
+++ Mosh YT/00.34.00.letter-position.py
@@ -0,0 +1,10 @@
+h0 = "Index"
+h1 = "Letter by Number"
+print(h1)
+course = "Python for Beginners"
+print(course[-2])  # this will print s (2 dari belakang)
+print(course[2])  # this will print y (2 dari depan)
+h2 = "Letter by Number"
+print(h2)
+print(course[1:-1])
+# this will print huruf ke 1 (inget mulai dari 0 ngitungnya) dari kiri SAMPAI DENGAN 1 huruf dari kanan
